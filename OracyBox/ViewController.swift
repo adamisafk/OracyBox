6ea5d5dfffb8e1fcf3f92776c97dc1419b3bdedd
Adam Howells <adamisafk@gmail.com>
2020-02-27 15:00:50 +0000

    Initial Commit

---
+++ OracyBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OracyBox
+//
+//  Created by Adam Howells on 27/02/2020.
+//  Copyright © 2020 Adam Howells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ OracyBoxTests/OracyBoxTests.swift
@@ -0,0 +1,34 @@
+//
+//  OracyBoxTests.swift
+//  OracyBoxTests
+//
+//  Created by Adam Howells on 27/02/2020.
+//  Copyright © 2020 Adam Howells. All rights reserved.
+//
+
+import XCTest
+@testable import OracyBox
+
+class OracyBoxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
