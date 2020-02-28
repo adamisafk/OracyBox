564605b51f9ca9c8946cc82c5a135a97d8c223eb
Adam Howells <adamisafk@gmail.com>
2020-02-28 21:20:27 +0000

    Add assets

---
+++ OracyBox/Supporting Files/UIColor+NamedColors.swift
@@ -0,0 +1,629 @@
+//
+// UIColor+NamedColors.swift
+//
+//
+// Created by Sketch Export for Xcode on 28 February 2020
+// Exported from file Storyboard.sketch
+//
+
+import UIKit
+
+extension UIColor {
+	// MARK: - Document Colors
+
+	static var darkMountainMeadowCanada: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "darkMountainMeadowCanada") ?? .white
+		} else {
+			return UIColor(red: 0.0627451, green: 0.6745098, blue: 0.5176471, alpha: 1.0)
+		}
+	}
+
+	static var greenSeaV1: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "greenSeaV1") ?? .white
+		} else {
+			return UIColor(red: 0.08627451, green: 0.627451, blue: 0.52156866, alpha: 1.0)
+		}
+	}
+
+
+	// MARK: - Login
+
+	static var capeHope: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "capeHope") ?? .white
+		} else {
+			return UIColor(red: 0.847, green: 0.847, blue: 0.847, alpha: 1.0)
+		}
+	}
+
+	static var thought: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "thought") ?? .white
+		} else {
+			return UIColor(red: 0.84313726, green: 0.8, blue: 0.78431374, alpha: 1.0)
+		}
+	}
+
+	static var honeyWax: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "honeyWax") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.654902, blue: 0.14901961, alpha: 1.0)
+		}
+	}
+
+	static var pollen: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "pollen") ?? .white
+		} else {
+			return UIColor(red: 0.9490196, green: 0.92941177, blue: 0.6666667, alpha: 1.0)
+		}
+	}
+
+	static var waterMist: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "waterMist") ?? .white
+		} else {
+			return UIColor(red: 0.78039217, green: 0.84313726, blue: 0.8862745, alpha: 1.0)
+		}
+	}
+
+	static var helloSummer: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "helloSummer") ?? .white
+		} else {
+			return UIColor(red: 0.30980393, green: 0.7647059, blue: 0.96862745, alpha: 1.0)
+		}
+	}
+
+	static var white: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "white") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+		}
+	}
+
+	static var shadedSun: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "shadedSun") ?? .white
+		} else {
+			return UIColor(red: 0.95686275, green: 0.9411765, blue: 0.6431373, alpha: 1.0)
+		}
+	}
+
+	static var softImpala: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "softImpala") ?? .white
+		} else {
+			return UIColor(red: 0.6313726, green: 0.53333336, blue: 0.49803922, alpha: 1.0)
+		}
+	}
+
+	static var dandelionWhisper: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "dandelionWhisper") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.9254902, blue: 0.7019608, alpha: 1.0)
+		}
+	}
+
+	static var vermilionBird: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "vermilionBird") ?? .white
+		} else {
+			return UIColor(red: 0.95686275, green: 0.2627451, blue: 0.21176471, alpha: 1.0)
+		}
+	}
+
+	static var scubaBlue: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "scubaBlue") ?? .white
+		} else {
+			return UIColor(red: 0.0, green: 0.6745098, blue: 0.75686276, alpha: 1.0)
+		}
+	}
+
+	static var honeyCrustedChicken: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "honeyCrustedChicken") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.7294118, blue: 0.34117648, alpha: 1.0)
+		}
+	}
+
+	static var blueViolet: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "blueViolet") ?? .white
+		} else {
+			return UIColor(red: 0.24705882, green: 0.31764707, blue: 0.70980394, alpha: 1.0)
+		}
+	}
+
+	static var brilliant: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "brilliant") ?? .white
+		} else {
+			return UIColor(red: 0.0, green: 0.5921569, blue: 0.654902, alpha: 1.0)
+		}
+	}
+
+	static var mushroom: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "mushroom") ?? .white
+		} else {
+			return UIColor(red: 0.7372549, green: 0.6666667, blue: 0.6431373, alpha: 1.0)
+		}
+	}
+
+	static var harāGreen: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "harāGreen") ?? .white
+		} else {
+			return UIColor(red: 0.29803923, green: 0.6862745, blue: 0.3137255, alpha: 1.0)
+		}
+	}
+
+	static var froly: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "froly") ?? .white
+		} else {
+			return UIColor(red: 0.8980392, green: 0.4509804, blue: 0.4509804, alpha: 1.0)
+		}
+	}
+
+	static var tiānLánSky: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "tiānLánSky") ?? .white
+		} else {
+			return UIColor(red: 0.39215687, green: 0.70980394, blue: 0.9647059, alpha: 1.0)
+		}
+	}
+
+	static var veganMastermind: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "veganMastermind") ?? .white
+		} else {
+			return UIColor(red: 0.15294118, green: 0.7294118, blue: 0.3019608, alpha: 1.0)
+		}
+	}
+
+	static var pinkSpyro: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "pinkSpyro") ?? .white
+		} else {
+			return UIColor(red: 0.6117647, green: 0.15294118, blue: 0.6901961, alpha: 1.0)
+		}
+	}
+
+	static var frightNight: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "frightNight") ?? .white
+		} else {
+			return UIColor(red: 0.050980393, green: 0.2784314, blue: 0.6313726, alpha: 1.0)
+		}
+	}
+
+	static var parachuting: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "parachuting") ?? .white
+		} else {
+			return UIColor(red: 0.003921569, green: 0.34117648, blue: 0.60784316, alpha: 1.0)
+		}
+	}
+
+	static var habaneroGold: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "habaneroGold") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.8352941, blue: 0.30980393, alpha: 1.0)
+		}
+	}
+
+	static var pacificOcean: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "pacificOcean") ?? .white
+		} else {
+			return UIColor(red: 0.5647059, green: 0.7921569, blue: 0.9764706, alpha: 1.0)
+		}
+	}
+
+	static var woodLake: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "woodLake") ?? .white
+		} else {
+			return UIColor(red: 0.6117647, green: 0.5058824, blue: 0.46666667, alpha: 1.0)
+		}
+	}
+
+	static var lineage: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "lineage") ?? .white
+		} else {
+			return UIColor(red: 0.30588236, green: 0.20392157, blue: 0.18039216, alpha: 1.0)
+		}
+	}
+
+	static var amour: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "amour") ?? .white
+		} else {
+			return UIColor(red: 0.9372549, green: 0.3254902, blue: 0.3137255, alpha: 1.0)
+		}
+	}
+
+	static var thunderboltBlue: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "thunderboltBlue") ?? .white
+		} else {
+			return UIColor(red: 0.27058825, green: 0.29411766, blue: 0.32941177, alpha: 1.0)
+		}
+	}
+
+	static var vitaminC: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "vitaminC") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.59607846, blue: 0.0, alpha: 1.0)
+		}
+	}
+
+	static var superSilver: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "superSilver") ?? .white
+		} else {
+			return UIColor(red: 0.93333334, green: 0.93333334, blue: 0.93333334, alpha: 1.0)
+		}
+	}
+
+	static var marigold: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "marigold") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.75686276, blue: 0.02745098, alpha: 1.0)
+		}
+	}
+
+	static var sun: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "sun") ?? .white
+		} else {
+			return UIColor(red: 0.9372549, green: 0.5529412, blue: 0.23529412, alpha: 1.0)
+		}
+	}
+
+	static var eggYolkYellow: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "eggYolkYellow") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.8, blue: 0.5019608, alpha: 1.0)
+		}
+	}
+
+	static var chimneySweep: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "chimneySweep") ?? .white
+		} else {
+			return UIColor(red: 0.14901961, green: 0.19607843, blue: 0.21960784, alpha: 1.0)
+		}
+	}
+
+	static var roxyBrown: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "roxyBrown") ?? .white
+		} else {
+			return UIColor(red: 0.4745098, green: 0.33333334, blue: 0.28235295, alpha: 1.0)
+		}
+	}
+
+	static var brassTrumpet: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "brassTrumpet") ?? .white
+		} else {
+			return UIColor(red: 0.92156863, green: 0.68235296, blue: 0.33333334, alpha: 1.0)
+		}
+	}
+
+	static var whaleShark: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "whaleShark") ?? .white
+		} else {
+			return UIColor(red: 0.3764706, green: 0.49019608, blue: 0.54509807, alpha: 1.0)
+		}
+	}
+
+	static var defenestration: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "defenestration") ?? .white
+		} else {
+			return UIColor(red: 0.78431374, green: 0.8392157, blue: 0.8980392, alpha: 1.0)
+		}
+	}
+
+	static var báiSèWhite: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "báiSèWhite") ?? .white
+		} else {
+			return UIColor(red: 0.9254902, green: 0.9411765, blue: 0.94509804, alpha: 1.0)
+		}
+	}
+
+	static var homeWorld: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "homeWorld") ?? .white
+		} else {
+			return UIColor(red: 0.11764706, green: 0.53333336, blue: 0.8980392, alpha: 1.0)
+		}
+	}
+
+	static var arabicCoffee: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "arabicCoffee") ?? .white
+		} else {
+			return UIColor(red: 0.42745098, green: 0.29803923, blue: 0.25490198, alpha: 1.0)
+		}
+	}
+
+	static var eclecticPlum: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "eclecticPlum") ?? .white
+		} else {
+			return UIColor(red: 0.5529412, green: 0.43137255, blue: 0.3882353, alpha: 1.0)
+		}
+	}
+
+	static var uclaGold: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "uclaGold") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.7019608, blue: 0.0, alpha: 1.0)
+		}
+	}
+
+	static var roseMadder: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "roseMadder") ?? .white
+		} else {
+			return UIColor(red: 0.8980392, green: 0.22352941, blue: 0.20784314, alpha: 1.0)
+		}
+	}
+
+	static var englishWalnut: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "englishWalnut") ?? .white
+		} else {
+			return UIColor(red: 0.24313726, green: 0.15294118, blue: 0.13725491, alpha: 1.0)
+		}
+	}
+
+	static var promCorsage: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "promCorsage") ?? .white
+		} else {
+			return UIColor(red: 0.88235295, green: 0.74509805, blue: 0.90588236, alpha: 1.0)
+		}
+	}
+
+	static var notYoCheese: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "notYoCheese") ?? .white
+		} else {
+			return UIColor(red: 0.9843137, green: 0.7529412, blue: 0.1764706, alpha: 1.0)
+		}
+	}
+
+	static var nasturcianFlower: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "nasturcianFlower") ?? .white
+		} else {
+			return UIColor(red: 0.9019608, green: 0.2901961, blue: 0.09803922, alpha: 1.0)
+		}
+	}
+
+	static var floralLeaf: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "floralLeaf") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.7176471, blue: 0.3019608, alpha: 1.0)
+		}
+	}
+
+	static var buttonBlue: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "buttonBlue") ?? .white
+		} else {
+			return UIColor(red: 0.12941177, green: 0.5882353, blue: 0.9529412, alpha: 1.0)
+		}
+	}
+
+	static var darkMountainMeadow: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "darkMountainMeadow") ?? .white
+		} else {
+			return UIColor(red: 0.0627451, green: 0.6745098, blue: 0.5176471, alpha: 1.0)
+		}
+	}
+
+	static var báiSèWhite1: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "báiSèWhite1") ?? .white
+		} else {
+			return UIColor(red: 0.9254902, green: 0.9372549, blue: 0.94509804, alpha: 1.0)
+		}
+	}
+
+	static var bulgarianRose: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "bulgarianRose") ?? .white
+		} else {
+			return UIColor(red: 0.29803923, green: 0.03137255, blue: 0.03137255, alpha: 1.0)
+		}
+	}
+
+	static var prisonJumpsuit: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "prisonJumpsuit") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.6666667, blue: 0.27058825, alpha: 1.0)
+		}
+	}
+
+	static var coquelicot: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "coquelicot") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.23921569, blue: 0.0, alpha: 1.0)
+		}
+	}
+
+	static var therapeuticToucan: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "therapeuticToucan") ?? .white
+		} else {
+			return UIColor(red: 0.9607843, green: 0.49803922, blue: 0.09019608, alpha: 1.0)
+		}
+	}
+
+	static var black: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "black") ?? .white
+		} else {
+			return UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+		}
+	}
+
+	static var suaveGrey: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "suaveGrey") ?? .white
+		} else {
+			return UIColor(red: 0.8117647, green: 0.84705883, blue: 0.8627451, alpha: 1.0)
+		}
+	}
+
+	static var whiteAlyssum: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "whiteAlyssum") ?? .white
+		} else {
+			return UIColor(red: 0.9372549, green: 0.92156863, blue: 0.9137255, alpha: 1.0)
+		}
+	}
+
+	static var morroBay: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "morroBay") ?? .white
+		} else {
+			return UIColor(red: 0.32941177, green: 0.43137255, blue: 0.47843137, alpha: 1.0)
+		}
+	}
+
+	static var helloSpring: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "helloSpring") ?? .white
+		} else {
+			return UIColor(red: 0.29803923, green: 0.8509804, blue: 0.39215687, alpha: 1.0)
+		}
+	}
+
+	static var snapShot: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "snapShot") ?? .white
+		} else {
+			return UIColor(red: 0.17254902, green: 0.24313726, blue: 0.3137255, alpha: 1.0)
+		}
+	}
+
+	static var tetsuIron: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "tetsuIron") ?? .white
+		} else {
+			return UIColor(red: 0.27058825, green: 0.3529412, blue: 0.39215687, alpha: 1.0)
+		}
+	}
+
+	static var whiteAlpha40: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "whiteAlpha40") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.4)
+		}
+	}
+
+	static var cadetGrey: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "cadetGrey") ?? .white
+		} else {
+			return UIColor(red: 0.5647059, green: 0.6431373, blue: 0.68235296, alpha: 1.0)
+		}
+	}
+
+	static var mountainAsh: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "mountainAsh") ?? .white
+		} else {
+			return UIColor(red: 0.78039217, green: 0.4627451, blue: 0.0, alpha: 1.0)
+		}
+	}
+
+	static var flamboyant: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "flamboyant") ?? .white
+		} else {
+			return UIColor(red: 0.08627451, green: 0.627451, blue: 0.52156866, alpha: 1.0)
+		}
+	}
+
+	static var ditheredSky: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "ditheredSky") ?? .white
+		} else {
+			return UIColor(red: 0.73333335, green: 0.87058824, blue: 0.9843137, alpha: 1.0)
+		}
+	}
+
+	static var pinkishOrange: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "pinkishOrange") ?? .white
+		} else {
+			return UIColor(red: 1.0, green: 0.45882353, blue: 0.29803923, alpha: 1.0)
+		}
+	}
+
+	static var xv88: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "xv88") ?? .white
+		} else {
+			return UIColor(red: 0.47058824, green: 0.2784314, blue: 0.09803922, alpha: 1.0)
+		}
+	}
+
+	static var chiGong: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "chiGong") ?? .white
+		} else {
+			return UIColor(red: 0.827451, green: 0.18431373, blue: 0.18431373, alpha: 1.0)
+		}
+	}
+
+	static var whiteSolid: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "whiteSolid") ?? .white
+		} else {
+			return UIColor(red: 0.9607843, green: 0.9647059, blue: 0.98039216, alpha: 1.0)
+		}
+	}
+
+
+	// MARK: - Symbols
+
+	static var blackAlpha59: UIColor {
+		if #available(iOS 11.0, *) {
+			return UIColor(named: "blackAlpha59") ?? .white
+		} else {
+			return UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.59999996)
+		}
+	}
+
+
+}
